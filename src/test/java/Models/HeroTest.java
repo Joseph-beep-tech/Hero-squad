cb6e42fc08ddd657b6f8c8e35bb2d7d1c344efdc
Joseph-beep-tech <Josephcluff006@gmail.com>
2020-11-29 13:45:17 +0300

    test for age

--- src/test/java/Models/HeroTest.java
+++ src/test/java/Models/HeroTest.java
@@ -5,4 +5,14 @@ import static org.junit.Assert.*;
 
 public class HeroTest{
     Hero one = new Hero ("superman", 30, "x-ray vision", "kryptonite", 3);
+
+    @Test
+    public void getHeroesName(){
+        assertEquals ("superman", one.getHeroesName());
+    }
+
+    @Test
+    public void setAge(){
+        assertEquals (30, one.getAge ());
+    }
 }
