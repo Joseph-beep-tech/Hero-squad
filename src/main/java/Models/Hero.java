01518b6f5aab8a8d88549998aaf323bc493c23cd
Joseph-beep-tech <Josephcluff006@gmail.com>
2020-11-29 11:59:07 +0300

    primitive data types generated

--- src/main/java/Models/Hero.java
+++ src/main/java/Models/Hero.java
@@ -4,5 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
     public class Hero{
-
+        private String heroesName;
+        private String superPower;
+        private String weakness;
+        private int age;
+        private int id;
+        private int squadId;
     }
